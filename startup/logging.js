67106c3ffb87142fe5481c4a2c198dacbcdf74c5
Spyro Infosys <mithunadhikari40@gmail.com>
2021-06-21 23:19:55 +0545

    project name in the package.json is now chanaged

--- startup/logging.js
+++ startup/logging.js
@@ -10,9 +10,9 @@ module.exports = function() {
     throw ex;
   });
   
-  winston.add(winston.transports.File, { filename: 'logfile.log' });
-  winston.add(winston.transports.MongoDB, { 
-    db: 'mongodb://localhost/vidly',
-    level: 'info'
-  });  
+  // winston.add(winston.transports.File, { filename: 'logfile.log' });
+  // winston.add(winston.transports.MongoDB, { 
+  //   db: 'mongodb://localhost/vidly',
+  //   level: 'info'
+  // });  
 }
