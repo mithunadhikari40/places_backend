4e3aef4d3c535cdbe8b6147632384de17cc6df7f
Spyro Infosys <mithunadhikari40@gmail.com>
2021-06-21 23:00:50 +0545

    server url is now changed

--- startup/db.js
+++ startup/db.js
@@ -3,13 +3,22 @@ const mongoose = require('mongoose');
 
 //server
 
-//localhost
-
 module.exports = function() {
-  mongoose.connect('mongodb://localhost:27017/places', {
+  mongoose.connect('mongodb+srv://mithun:mithun@cluster0.r3i7g.mongodb.net/places', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false 
   })
     .then(() => winston.info('Connected to MongoDB...'));
 }
+
+//localhost
+
+// module.exports = function() {
+//   mongoose.connect('mongodb://localhost:27017/places', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false 
+//   })
+//     .then(() => winston.info('Connected to MongoDB...'));
+// }
